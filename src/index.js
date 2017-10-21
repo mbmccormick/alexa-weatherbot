5333a0815ba22f40583887dcabc1d73d087dba27
Matt McCormick <matt@mbmccormick.com>
2017-10-20 18:50:23 -0700

    Adjustments to forecast responses.

--- src/index.js
+++ src/index.js
@@ -41,10 +41,10 @@ var defaultHandler = {
                     var temperature = Math.round(data.currently.temperature);
                     var currently_summary = data.currently.summary;
                     var hourly_summary = data.hourly.summary;
-                    var high = Math.round(data.daily.data[0].temperatureMax);
-                    var low = Math.round(data.daily.data[0].temperatureMin);
+                    var high = timestamp <= data.daily.data[0].temperatureHighTime ? Math.round(data.daily.data[0].temperatureHigh) : Math.round(data.daily.data[1].temperatureHigh);
+                    var low = timestamp <= data.daily.data[0].temperatureLowTime ? Math.round(data.daily.data[0].temperatureLow) : Math.round(data.daily.data[1].temperatureLow);
 
-                    _alexa.emit(":tell", "Welcome to Weatherbot! Right now in " + location + ", it's " + temperature + " degrees and " + currently_summary + ". Today, the forecast is " + hourly_summary + ", with a high of " + high + " degrees and a low of " + low + " degrees." + getWeatherAlerts(data));
+                    _alexa.emit(":tell", "Welcome to Weatherbot! Right now in " + location + ", it's " + temperature + " degrees and " + currently_summary + ". The forecast for the next 24 hours is " + hourly_summary + ", with a high of " + high + " degrees and a low of " + low + " degrees." + getWeatherAlerts(data));
                 });
             });
         });
@@ -76,8 +76,8 @@ var defaultHandler = {
             getRequestedDateTime(_alexa, timezone, function (timestamp, difference, calendarTime) {
                 getForecast(_alexa, latitude, longitude, difference == 0 ? null : timestamp, function (data) {
                     var summary = data.daily.data[0].summary;
-                    var high = Math.round(data.daily.data[0].temperatureMax);
-                    var low = Math.round(data.daily.data[0].temperatureMin);
+                    var high = Math.round(data.daily.data[0].temperatureHigh);
+                    var low = Math.round(data.daily.data[0].temperatureLow);
 
                     if (difference == 0) {
                         _alexa.emit(":tell", "The forecast for Today in " + location + " is " + summary + " with a high of " + high + " degrees and a low of " + low + " degrees." + getWeatherAlerts(data));
@@ -180,8 +180,8 @@ var defaultHandler = {
         getRequestedLocation(_alexa, function (latitude, longitude, location, timezone) {
             getRequestedDateTime(_alexa, timezone, function (timestamp, difference, calendarTime) {
                 getForecast(_alexa, latitude, longitude, difference == 0 ? null : timestamp, function (data) {
-                    var high = Math.round(data.daily.data[0].temperatureMax);
-                    var timestamp = Moment.unix(data.daily.data[0].temperatureMaxTime).tz(timezone);
+                    var high = Math.round(data.daily.data[0].temperatureHigh);
+                    var timestamp = Moment.unix(data.daily.data[0].temperatureHighTime).tz(timezone);
 
                     var difference = timestamp.diff(Moment.tz(timezone), "hours");
 
@@ -210,8 +210,8 @@ var defaultHandler = {
         getRequestedLocation(_alexa, function (latitude, longitude, location, timezone) {
             getRequestedDateTime(_alexa, timezone, function (timestamp, difference, calendarTime) {
                 getForecast(_alexa, latitude, longitude, difference == 0 ? null : timestamp, function (data) {
-                    var low = Math.round(data.daily.data[0].temperatureMin);
-                    var timestamp = Moment.unix(data.daily.data[0].temperatureMinTime).tz(timezone);
+                    var low = Math.round(data.daily.data[0].temperatureLow);
+                    var timestamp = Moment.unix(data.daily.data[0].temperatureLowTime).tz(timezone);
 
                     var difference = timestamp.diff(Moment.tz(timezone), "hours");
 
@@ -777,7 +777,7 @@ function getWeatherAlerts(data) {
             response += " A weather alert is in effect for this area.";
         }
 
-        return response + " If you'd like to know more, just ask me for your weather alerts.";
+        return response + " If you'd like to know more, just ask for your weather alerts.";
     }
     else {
         return "";
